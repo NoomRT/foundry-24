913b1211f0518d74c0d6262ccf3ddf36aa0a3e38
NoomRT <rueangyotnoom@gmail.com>
2024-08-11 21:48:34 +0700

    Refactor make our code readable

--- foundry-fund-me-f24/script/HelperConfig.s.sol
+++ foundry-fund-me-f24/script/HelperConfig.s.sol
@@ -15,6 +15,10 @@ contract HelperConfig is Script {
     // Otherwise, grab the existing address from the live network
     NetworkConfig public activeNetworkConfig;
 
+    // Refactor make our code readable
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
     struct NetworkConfig {
         address priceFeed; // ETH/USD price feed address
     }
@@ -55,7 +59,7 @@ contract HelperConfig is Script {
         // 2. Return the mock address
 
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
         vm.stopBroadcast();
 
         NetworkConfig memory anvilConfig = NetworkConfig({
