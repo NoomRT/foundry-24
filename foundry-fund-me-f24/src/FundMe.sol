df901666f32784cc4e54db26cca6d18d68d91171
NoomRT <rueangyotnoom@gmail.com>
2024-08-12 15:47:40 +0700

    Update FundMe

--- foundry-fund-me-f24/src/FundMe.sol
+++ foundry-fund-me-f24/src/FundMe.sol
@@ -17,7 +17,7 @@ contract FundMe {
     address[] private s_funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
+    address private immutable i_owner;
     uint256 public constant MINIMUM_USD = 5e18;
     AggregatorV3Interface private s_priceFeed;
 
@@ -78,6 +78,22 @@ contract FundMe {
 
     receive() external payable {
         fund();
+    }
+
+    /**
+     * View / Pure functions (Getters)
+     */
+
+    function getAddressToAmountFunded(address fundingAddress) external view returns(uint256){
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunders(uint256 index) external view returns(address) {
+        return s_funders[index];
+    }
+
+    function getOwner() external view returns(address) {
+        return i_owner;
     }
 }
 
