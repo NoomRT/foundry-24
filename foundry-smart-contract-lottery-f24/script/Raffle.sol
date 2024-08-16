3f97824d22741ab8d053630bc6abba9e3fe0999e
NoomRT <rueangyotnoom@gmail.com>
2024-08-16 20:59:10 +0700

    modules

---
+++ foundry-smart-contract-lottery-f24/script/Raffle.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+/**
+ * @title A sample Raffle contract
+ * @author Rueangyot Treemat
+ * @notice This contract is for creating a sample raffle
+ * @dev Implement Chainlink VRFv2.5
+ */
+contract Raffle {
+
+    uint256 private immutable entranceFee;
+
+    function enterRaffle() public {
+
+    }
+
+    function pickWinner() public {
+
+    }
+}
